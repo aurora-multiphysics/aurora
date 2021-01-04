25a4585bc40a83ec00fdc4f1094443d961336a65
helen-brooks <helen.brooks@ukaea.uk>
2021-01-04 15:46:59 +0000

    Add a clarifying comment to OpenMCProblem

--- openmc/include/problem/OpenMCProblem.h
+++ openmc/include/problem/OpenMCProblem.h
@@ -7,6 +7,11 @@ class OpenMCProblem;
 template <>
 InputParameters validParams<OpenMCProblem>();
 
+// Minimal definition for a FE problem.
+// Only included as it is required by Moose
+// and our constructro avoids unneccessary initialisation.
+// Our executioner does not actually call these methods
+//  - it will not successfully run for a normal moose executioner.
 class OpenMCProblem : public ExternalProblem
 {
  public:
@@ -15,6 +20,6 @@ class OpenMCProblem : public ExternalProblem
   
   virtual void externalSolve () override {};  
   virtual bool converged () override { return true; };  
-  virtual void syncSolutions(Direction /*direction*/) override {};;
+  virtual void syncSolutions(Direction /*direction*/) override {};
 
 };
