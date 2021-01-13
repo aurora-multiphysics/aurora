3a3c18eb5efedbf599ee87a34e5d6f05d423f84e
helen-brooks <helen.brooks@ukaea.uk>
2021-01-13 15:15:35 +0000

    Agnosticise registryTest from name of app

--- openmc/unit/src/RegistryTest.C
+++ openmc/unit/src/RegistryTest.C
@@ -11,27 +11,31 @@
 
 TEST_F(OpenMCAppBasicTest, registryTest)
 {
+  std::vector<std::string> knownObjNames;
+  knownObjNames.push_back("OpenMCProblem");
+  knownObjNames.push_back("OpenMCExecutioner");
+  knownObjNames.push_back("MoabUserObject");
 
   ASSERT_FALSE(appIsNull);
 
   // Fetch a reference to all objects that been registered (in the global singleton)
   const auto& allRegistered = Registry::allObjects();
 
-  // Check OpenMCApp exists in the registry
-  bool foundOpenMC = allRegistered.find("OpenMCApp") != allRegistered.end();
-  ASSERT_TRUE(foundOpenMC);
+  // Check the app exists in the registry
+  bool foundApp = allRegistered.find(appName) != allRegistered.end();
+  ASSERT_TRUE(foundApp);
 
-  // Get all objects registered to OpenMC
-  const auto& auroraObjs = allRegistered.at("OpenMCApp");
+  // Get all objects registered to the app
+  const auto& appObjs = allRegistered.at(appName);
 
   // Create a list of names of all objects we expect to see registered
   std::map<std::string,bool> knownObjs;
-  knownObjs["OpenMCProblem"] = false;
-  knownObjs["OpenMCExecutioner"] = false;
-  knownObjs["MoabUserObject"] = false;
+  for(const auto objName : knownObjNames){
+    knownObjs[objName] = false;
+  }
 
   // Check the objects match up
-  for( const auto & obj : auroraObjs) {
+  for( const auto & obj : appObjs) {
     std::string objName = obj._classname;
     bool isKnownObj = knownObjs.find(objName) != knownObjs.end();
     EXPECT_TRUE(isKnownObj) << "Unknown object was registered";
@@ -47,7 +51,7 @@ TEST_F(OpenMCAppBasicTest, registryTest)
   // Now should have found all known objects
   for( const auto & knownObj : knownObjs){
     EXPECT_TRUE(knownObj.second) << "Object "<< knownObj.first
-                                 << " was not registered to OpenMCApp.";
+                                 << " was not registered to "<<  appName;
   }
 
 }
