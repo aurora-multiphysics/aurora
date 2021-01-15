86e4734fbc274812564b7a4de31d341ecf5b5a74
helen-brooks <helen.brooks@ukaea.uk>
2021-01-15 10:10:44 +0000

    Extend FunctionUserObjectTest to cover edge cases

--- unit/src/FunctionUserObjectTest.C
+++ unit/src/FunctionUserObjectTest.C
@@ -120,6 +120,9 @@ TEST_F(FunctionUserObjectTest, checkSolution)
   // Set the solution
   ASSERT_TRUE(moabUOPtr->setSolution(var_name,results,1.0,false));
 
+  // Get the mesh
+  MeshBase& mesh = problemPtr->mesh().getMesh();
+
   // Now loop over the centroids and check function at each point
   size_t nPoints = centroids.size();
   for(size_t iPoint=0; iPoint<nPoints; iPoint++){
@@ -131,6 +134,18 @@ TEST_F(FunctionUserObjectTest, checkSolution)
     // Check
     double diff = fabs(function_result-result);
     EXPECT_LE(diff,tol);
+
+    // Now check the nodes of the elem whose centroid this is
+    // to test edge cases
+    const Elem & elem=mesh.elem_ref(iPoint);
+    unsigned int nNodes = elem.n_nodes();
+    for(unsigned int iNode=0; iNode<nNodes; iNode++){
+      Point pNode = elem.point(iNode);
+      double node_result;
+      EXPECT_NO_THROW(evalFunction(pNode,node_result));
+      double nodediff = fabs(node_result-result);
+      EXPECT_LE(diff,tol) << "Result differs for node "<< iNode;
+    }
   }
 
   // Now check a point outside the domain
