1442e9da85b2b73dc8b774de4757011434bfe24e
helen-brooks <helen.brooks@ukaea.uk>
2021-01-27 16:08:49 +0000

    A little refactoring for python performance analysis script

--- openmc/performance/process_perf_results.py
+++ openmc/performance/process_perf_results.py
@@ -3,6 +3,19 @@
 import matplotlib.pyplot as plt
 import matplotlib.gridspec as gridspec
 
+def get_csv_data(filename):
+    timers={}
+    with open(filename, "r") as f:
+        line = f.readline()
+        line = line.rstrip()
+        timer_keys = line.split(",")
+        line = f.readline()
+        line = line.rstrip()
+        times = line.split(",")
+        timers=dict(zip(timer_keys,times))
+    return timers
+
+
 filebase="perf_test"
 procs=[1,2,4]
 parts=[100,1000,10000]
@@ -19,25 +32,18 @@ for nmpi in procs:
         # Define csv filename
         filename=filebase+"_np"+str(np)+"_nmpi"+str(nmpi)+"_nt"+str(nthreads)+".csv"
         # Extract data from file
-        with open(filename, "r") as f:
-            line = f.readline()
-            line = line.rstrip()
-            timer_keys = line.split(",")
-            line = f.readline()
-            line = line.rstrip()
-            times = line.split(",")
-            timers=dict(zip(timer_keys,times))
+        timers=get_csv_data(filename)
 
-            for key,time in timers.items():
-                if not key in datasets :
-                    datasets[key]=[]
-                datasets[key].append(float(time))
-        
-    
+        for key,time in timers.items():
+            if not key in datasets :
+                datasets[key]=[]
+            datasets[key].append(float(time))
+
+
     label="# MPI = "+str(nmpi)
     labels.append(label)
     all_datasets.append(datasets)
-    
+
 # Plot specifications
 
 #Plot title
