4a3d7f2a32072a6c6030bf7b612d87a65df1c530
helen-brooks <helen.brooks@ukaea.uk>
2021-01-28 09:51:52 +0000

    Add openmc data to performance plot

--- openmc/performance/process_perf_results.py
+++ openmc/performance/process_perf_results.py
@@ -19,6 +19,8 @@ def append_datasets(timers,datasets):
     for key,time in timers.items():
         #Strip quotes
         key=key.strip("\"")
+        if key=="":
+            continue
         if not key in datasets :
             datasets[key]=[]
         datasets[key].append(float(time))
@@ -33,6 +35,7 @@ def append_datasets_from_file(filebase,np,nmpi,nthreads,ext,datasets):
 
 # Patterns for data files
 filebase="perf_test"
+comparefilebase="openmc_perf_test"
 ext=".csv"
 
 # Run specifications
@@ -42,25 +45,38 @@ nthreads=1
 
 # Get all the performance data in a dictionary
 all_datasets=[]
+all_cmpdatasets=[]
+
+# Place to keep legend text
 labels=[]
+cmplabels=[]
 
 for nmpi in procs:
 
     # Dictionaries to store the data for different numbers of processes
     datasets={}
+    cmpdatasets={}
 
     # Loop over runs with different numbers of particles
     for np in parts:
         append_datasets_from_file(filebase,np,nmpi,nthreads,ext,datasets)
+        append_datasets_from_file(comparefilebase,np,nmpi,nthreads,ext,cmpdatasets)
 
-    label="# MPI = "+str(nmpi)
+    # Create legend lables
+    labelbase="# MPI = "+str(nmpi)
+    label=labelbase+" (MOOSE)"
+    cmplabel=labelbase+" (OpenMC)"
     labels.append(label)
+    cmplabels.append(cmplabel)
+
+    # Save all the data
     all_datasets.append(datasets)
+    all_cmpdatasets.append(cmpdatasets)
 
 # Plot specifications
 
 #Plot title
-title="Time spent in OpenMCExecutioner::run"
+title="Time spent in OpenMCExecutioner::run vs. openmc_run"
 x_label="# Particles"
 y_label="Time / s"
 markers=["^","o","s"]
@@ -68,19 +84,29 @@ colours=["red","green","blue"]
 
 # Specify the dataset we want
 dataset_name="executioner_run_time"
+cmpdataset_name="simulation"
 
 # Specify an output name
 outfile=dataset_name+".png"
 
 # Create a new plot
 fig = plt.figure(tight_layout=True)
-gs = gridspec.GridSpec(1,1)
+gs = gridspec.GridSpec(1,2)
+
+#fig, axs = plt.subplots(1, 2, constrained_layout=True)
+
 
 ax=fig.add_subplot(gs[0])
+ax2=fig.add_subplot(gs[1])
 ax.set_xlabel(x_label)
 ax.set_ylabel(y_label)
 ax.set_xscale("log")
 ax.set_yscale("log")
+
+ax2.set_xlabel(x_label)
+ax2.set_ylabel(y_label)
+ax2.set_xscale("log")
+ax2.set_yscale("log")
 
 nsets = len(all_datasets)
 
@@ -88,8 +114,13 @@ for iset in range(nsets):
     data=all_datasets[iset][dataset_name]
     ax.scatter(parts,data,c=colours[iset],marker=markers[iset],label=labels[iset])
 
-plt.legend(loc='upper left');
-plt.title(title)
+    cmpdata=all_cmpdatasets[iset][cmpdataset_name]
+    ax2.scatter(parts,cmpdata,edgecolors=colours[iset],facecolors='none',marker=markers[iset],label=cmplabels[iset])
+
+
+ax.legend(loc='upper left');
+ax2.legend(loc='upper left');
+fig.suptitle(title)
 
 # Save
 plt.savefig(outfile)
