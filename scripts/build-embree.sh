8d47e07e623aec2feeacd3e457bb336bf92393f4
helen-brooks <helen.brooks@ukaea.uk>
2022-10-25 13:29:25 +0100

    Add script to build embree

--- scripts/build-embree.sh
+++ scripts/build-embree.sh
@@ -11,41 +11,66 @@ Help()
     echo "h      Print this help."
     echo "w      Set working directory"
     echo "i      Set installation directory, i.e. installs to  INSTALLDIR/$PACKAGE (default: $INSTALLDIR)"
-    echo "t      Set moab tag check out  (default: $MOABTAG)"
-    echo "d      Provide HDF5 library installation directory (default: $HDF5LIBDIR)"
+    echo "t      Set tag check out  (default: $TAG)"
     echo "j      Set number of build jobs  (default: $JOBS)"
     echo "e      Set file from which to source environment"
-    echo "o      Set file in which to create MOAB profile (default:${ENV_OUTFILE} )"
+    echo "o      Set file in which to create output profile (default:${ENV_OUTFILE} )"
     echo
 }
 
-build_moab()
+setup_env()
+{
+if [ "$WORKDIR" = "" ] ; then
+    echo "Please set working directory through the -w flag"
+    Help
+    exit 1
+fi
+SRC_STR=""
+if [ -n "$ENV_FILE" ]; then
+    echo "Sourcing environment from $ENV_FILE"
+    source $ENV_FILE
+    SRC_STR="source $ENV_FILE"
+fi
+
+#if [ -f $ENV_OUTFILE ]; then
+#   echo "File $ENV_OUTFILE already exists!"
+#   exit 1
+#fi
+
+## Create a profile
+#echo "Creating profile in ${ENV_OUTFILE}"
+#touch $ENV_OUTFILE
+#if [ -n "$SRC_STR" ]; then
+#    echo $SRC_STR >>  $ENV_OUTFILE
+#fi
+}
+
+build_package()
 {
     # Package setup
     ORIGDIR=$PWD
     PACKAGE_DIR="$PACKAGE-bld"
     PACKAGE_INSTALL_DIR="$INSTALLDIR/$PACKAGE"
-    
+
     # Place to build
-    echo "Building MOAB in $WORKDIR/${PACKAGE_DIR}"
-    cd $WORKDIR    
+    echo "Building $PACKAGE in $WORKDIR/${PACKAGE_DIR}"
+    cd $WORKDIR
     if [ ! -d ${PACKAGE_DIR} ]; then
         mkdir ${PACKAGE_DIR}
     fi
     cd ${PACKAGE_DIR}
-    
+
     # Get the source code
-    if [ ! -d $PACKAGE  ]; then
-        git clone https://bitbucket.org/fathomteam/moab
-        cd $PACKAGE
+    if [ ! -d ${REPO_NAME}  ]; then
+        git clone ${PACKAGE_REPO}
+        cd ${REPO_NAME}
     else
-        cd $PACKAGE
+        cd ${REPO_NAME}
         git clean -xfd
     fi
-    git checkout $MOABTAG
-    autoreconf -fi
+    git checkout $TAG
     cd ../
-    
+
     # Create clean build directory
     if [ -d $BUILDDIR]; then
         rm -r $BUILDDIR
@@ -54,31 +79,34 @@ build_moab()
     cd $BUILDDIR
 
     # Configure
-    echo "Running configure in $BUILDDIR"
-    ../moab/configure --prefix=${PACKAGE_INSTALL_DIR} \
-                      --with-hdf5=$HDF5LIBDIR \
-                      --enable-optimize \
-                      --enable-shared \
-                      --disable-debug
+    echo "Running cmake in $BUILDDIR"
+    cmake ../${REPO_NAME}/ \
+          -DCMAKE_INSTALL_PREFIX=${PACKAGE_INSTALL_DIR} \
+          $ADDITIONAL_CMAKE_FLAGS
+
     # Build
     make -j$JOBS
-    make check
+
+    # Test
+    if [ -n "${RUN_TEST_CMD}" ]; then
+        eval $RUN_TEST_CMD
+    fi
+
+    # Install
     make install
 
     # Return to starting directory
     cd $ORIGDIR
 }
 
-# Defaults 
-PACKAGE=moab
+# Defaults
 INSTALLDIR=$HOME/local
 WORKDIR=""
-MOABTAG=Version5.2.0
+TAG=v3.6.1
 BUILDDIR=bld
-HDF5LIBDIR=/usr/lib/x86_64-linux-gnu/hdf5/serial/
 JOBS=1
 ENV_FILE=
-ENV_OUTFILE="$HOME/.moabprofile"
+ENV_OUTFILE="$HOME/.embreeprofile"
 
 # Read arguments
 while getopts "w:i:t:d:j:e:o:h" option; do
@@ -92,8 +120,6 @@ while getopts "w:i:t:d:j:e:o:h" option; do
             INSTALLDIR=$OPTARG;;
         t)
             TAG=$OPTARG;;
-        d)
-            HDF5LIBDIR=$OPTARG;;
         j)
             JOBS=$OPTARG;;
         e)
@@ -106,28 +132,11 @@ while getopts "w:i:t:d:j:e:o:h" option; do
     esac
 done
 
-if [ "$WORKDIR" = "" ] ; then
-    echo "Please set working directory through the -w flag"
-    Help
-    exit 1
-fi
-SRC_STR=""
-if [ -n "$ENV_FILE" ]; then
-    echo "Sourcing environment from $ENV_FILE"
-    source $ENV_FILE
-    SRC_STR="source $ENV_FILE"
-fi
+PACKAGE=embree
+REPO_NAME=embree
+PACKAGE_REPO=https://github.com/embree/embree.git
+ADDITIONAL_CMAKE_FLAGS="-DCMAKE_CXX_COMPILER=$CXX -DCMAKE_C_COMPILER=$CC -DEMBREE_ISPC_SUPPORT=0"
+RUN_TEST_CMD=""
 
-if [ -f $ENV_OUTFILE ]; then
-   echo "File $ENV_OUTFILE already exists!"
-   exit 1
-fi
-
-# Create a moab profile
-echo "Creating MOAB profile in ${ENV_OUTFILE}"
-touch $ENV_OUTFILE
-if [ -n "$SRC_STR" ]; then
-    echo $SRC_STR >>  $ENV_OUTFILE
-fi
-
-build_moab
+setup_env
+build_package
